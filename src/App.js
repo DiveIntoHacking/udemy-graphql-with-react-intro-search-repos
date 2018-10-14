15369f491e61eb2e4c1078064f2df6507216e5b1
Atsushi Ishida <gipcompany@gmail.com>
2018-10-15 02:50:23 +0900

    update totalCount with writeQuery

--- src/App.js
+++ src/App.js
@@ -13,7 +13,27 @@ const StarButton = props => {
       <button
         onClick={
           () => addOrRemoveStar({
-            variables: { input: { starrableId: node.id } }
+            variables: { input: { starrableId: node.id } },
+            update: (store, { data: { addStar, removeStar }}) => {
+              const { starrable } = addStar || removeStar
+              console.log(starrable)
+              const data = store.readQuery({
+                query: SEARCH_REPOSITORIES,
+                variables:  { query, first, last, after, before }
+              })
+              const edges = data.search.edges
+              const newEdges = edges.map(edge => {
+                if (edge.node.id === node.id) {
+                  const totalCount = edge.node.stargazers.totalCount
+                  const diff = starrable.viewerHasStarred ? 1 : -1
+                  const newTotalCount = totalCount + diff
+                  edge.node.stargazers.totalCount = newTotalCount
+                }
+                return edge
+              })
+              data.search.edges = newEdges
+              store.writeQuery({ query: SEARCH_REPOSITORIES, data })
+            }
           })
         }
       >
@@ -25,15 +45,6 @@ const StarButton = props => {
   return (
     <Mutation
       mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
-      refetchQueries={mutationResult => {
-        console.log({mutationResult})
-        return [
-          {
-            query: SEARCH_REPOSITORIES,
-            variables: { query, first, last, before, after }
-          }
-        ]
-      }}
     >
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar} />
